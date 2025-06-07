9b08e6228d1e0b4e0e727669c25aaed8565611e5
Nick Saurino <nicksaurinocruz@gmail.com>
2025-06-07 18:59:55 -0500

    Added application, bootstrap, eurekaclient to the project

--- src/main/java/com/ufostyle/noodlebackend/NoodleBackendApplication.java
+++ src/main/java/com/ufostyle/noodlebackend/NoodleBackendApplication.java
@@ -2,8 +2,10 @@ package com.ufostyle.noodlebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NoodleBackendApplication {
 
 	public static void main(String[] args) {
