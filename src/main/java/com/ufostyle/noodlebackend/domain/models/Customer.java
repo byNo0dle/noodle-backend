2a1ce29e659dca891b228898c02169844f584dd1
Nick Saurino <nicksaurinocruz@gmail.com>
2025-06-07 12:04:06 -0500

    Added jsonproperties for better structure in json

--- src/main/java/com/ufostyle/noodlebackend/domain/models/Customer.java
+++ src/main/java/com/ufostyle/noodlebackend/domain/models/Customer.java
@@ -1,6 +1,11 @@
 package com.ufostyle.noodlebackend.domain.models;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,22 +18,39 @@ public class Customer {
 
     @Id
     @GeneratedValue
+    @JsonProperty("customer_id")
     @Column(name = "customer_id", columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID customerId;
+
+    @JsonProperty("names")
     @Column(name = "customer_name", length = 40, nullable = false)
     private String names;
+
+    @JsonProperty("first_surname")
     @Column(name = "first_surname", length = 20, nullable = false)
     private String firstSurName;
+
+    @JsonProperty("second_last_name")
     @Column(name = "second_lastname", length = 20, nullable = false)
     private String secondLastName;
+
+    @JsonProperty("customer_dni")
     @Column(name = "dni", nullable = false)
     private Integer dni;
+
+    @JsonProperty("birth_date")
     @Column(name = "birth_date", nullable = false)
     private Date birthdate;
+
+    @JsonProperty("cell_phone_number")
     @Column(name = "cell_phone_number", nullable = false)
     private Integer cellPhoneNumber;
-    @Column(name = "gender_type", length = 9)
+
+    @JsonProperty("gender_type")
+    @Column(name = "gender_type", length = 9, nullable = false)
     private String genderType;
-    @Column(name = "customer_email", nullable = false)
+
+    @JsonProperty("customer_email")
+    @Column(name = "customer_email", length = 35, nullable = false)
     private String email;
 }
